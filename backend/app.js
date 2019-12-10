bd75e5d2e678b008284f64ac7276bed6b6b1dbe9
Simon du Preez <dupreez.s.m@gmail.com>
2019-12-10 22:08:10 +0000

    Add socket io

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,22 @@
 const Koa = require('koa');
+const http = require('http')
+const socketIO = require('socket.io');
+
 const app = new Koa();
 
 app.use(async ctx => {
   ctx.body = 'Hello World';
 });
 
-app.listen(3000);
+const server = http.createServer(app.callback());
+
+
+// Socket IO stuff
+const io = socketIO(server);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+
+server.listen(3000);
