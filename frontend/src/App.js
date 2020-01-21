60a76feca3908292347d0743264996837abd5873
Simon du Preez <dupreez.s.m@gmail.com>
2020-01-21 08:32:40 +0000

    Add routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
 import JoinOrCreate from 'pages/JoinOrCreate';
 import Lobby from 'pages/Lobby';
 import GameSession from 'pages/GameSession';
 
 function App() {
-  return <JoinOrCreate />;
-  // return <GameSession />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/join"><JoinOrCreate /></Route>
+        <Route path="/lobby"><Lobby /></Route>
+        <Route path="/session"><GameSession /></Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
